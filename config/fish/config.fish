71516797a59f57049782dbc05b9ab1aff3873b98
Chris Martin <ch.martin@gmail.com>
2015-04-18 21:27:54 -0700

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,9 @@
+set --erase fish_greeting
+
+set PATH $HOME/bin $HOME/dotfiles/bin $PATH
+
+source ~/.config/fish/virtual.fish
+
 alias emacs '/bin/emacs -nw'
 
 # https://twitter.com/chris__martin/status/420992421673988096
